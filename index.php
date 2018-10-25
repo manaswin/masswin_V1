072e0b63f2e3452f9cd841ae4ff1cf1739ced720
manaswin <member@manaswin.org>
2018-10-25 23:02:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Welcome to Masswin</title>
+	<link rel="stylesheet" href="assets/style.css" type="text/css" charset="utf-8" /> 
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<script language="JavaScript" src="menu-details.js"></script>
+<script language="JavaScript" src="mm_menu.js"></script>
+<style type="text/css">
+<!--
+.body {	text-align: center;
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+}
+-->
+</style>
+</head>
+
+<body>
+<script language="JavaScript1.2">mmLoadMenus();</script>
+<table width="960" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr align="center">
+        <td colspan="3"><object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="960" height="223">
+          <param name="movie" value="creatives/header-goal.swf" />
+          <param name="quality" value="high" />
+          <param name="wmode" value="opaque" />
+          <param name="swfversion" value="8.0.35.0" />
+          <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+          <param name="expressinstall" value="Scripts/expressInstall.swf" />
+          <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+          <!--[if !IE]>-->
+          <object type="application/x-shockwave-flash" data="creatives/header-goal.swf" width="960" height="223">
+            <!--<![endif]-->
+            <param name="quality" value="high" />
+            <param name="wmode" value="opaque" />
+            <param name="swfversion" value="8.0.35.0" />
+            <param name="expressinstall" value="Scripts/expressInstall.swf" />
+            <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+            <div>
+              <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+              <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+              </div>
+            <!--[if !IE]>-->
+            </object>
+          <!--<![endif]-->
+          </object></td>
+      </tr>
+      <tr align="center">
+        <td>&nbsp;</td>
+        <td bgcolor="#870100"><table width="100%" border="0">
+          <!--DropDownMenuDescription Give each menu a unique id to drop down from same location-->
+          <tr align="center" class="TopMenu">
+            <td><a href="index.shtml">Home</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="about-us-intro.htm" name="link1" id="link1" onmouseover="MM_showMenu(window.mm_menu_09835063972_0,0,18,null,'link1')" onmouseout="MM_startTimeout();">About GOAL</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="classroom.htm" name="link2" id="link2" onmouseover="MM_showMenu(window.mm_menu_09835661942_0,0,18,null,'link2')" onmouseout="MM_startTimeout();">COURSES OFFERED</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="classroom.htm" name="link3" id="link3" onmouseover="MM_showMenu(window.mm_menu_09334525474_0,0,18,null,'link3')" onmouseout="MM_startTimeout();">Success Story</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="classroom.htm" name="link4" id="link4" onmouseover="MM_showMenu(window.mm_menu_06123267199_0,0,18,null,'link4')" onmouseout="MM_startTimeout();">Results</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="http://www.goalpracticecentre.com/media/">Media Gallery</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="classroom.htm" name="link5" id="link5" onmouseover="MM_showMenu(window.mm_menu_06123267207_0,0,18,null,'link5')" onmouseout="MM_startTimeout();">Facilities</a><img src="images/spacer.gif" width="15" height="10" alt="spacer" /><a href="contact-us.htm">Contact Us</a></td>
+            </tr>
+          </table></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr align="center">
+        <td>&nbsp;</td>
+        <td><table width="100%" border="0" cellpadding="0" cellspacing="0" class="table-brdr">
+          <tr>
+            <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>
+                  <table width="100%" border="0" cellspacing="0" cellpadding="2">
+                    <tr>
+                      <td valign="top">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td><script language="JavaScript1.2" src="assets/menuitem-home.js" type="text/javascript"></script></td>
+                            </tr>
+                          </table>
+                        <img src="images/spacer.gif" width="150" height="2" alt="spacer" /></td>
+                      <td valign="top" class="content-box"><table width="100%" border="0" cellpadding="0" cellspacing="0" class="content-box">
+                        <tr>
+                          <td class="body_hdg">Search :: CBSE MAINS Result</td>
+                          </tr>
+                        <tr>
+                          <td class="body_1"><table width="100%" border="0" cellspacing="0" cellpadding="5">
+                            <tr>
+                              <td>
+                              <form method="post" action="result-cbse-code.php">
+                              <table width="600" border="0" align="center" cellpadding="4" cellspacing="4" id="AutoNumber1">
+                                <tr>
+                                  <td width="192" align="center">&nbsp;</td>
+                                  <td width="150" align="center">&nbsp;</td>
+                                  <td width="218" align="center">&nbsp;</td>
+                                  </tr>
+                                <tr>
+                                  <td align="right"><strong>Roll Number</strong></td>
+                                  <td align="center"><span style="margin: 4">
+                                    <input type="text" name="search" size="25" maxlength="25" />
+                                    </span></td>
+                                  <td align="left"><input type="submit" name="Submit" value="Get Result" /></td>
+                                </tr>
+                                <tr>
+                                  <td align="center">&nbsp;</td>
+                                  <td align="center">&nbsp;</td>
+                                  <td align="center">&nbsp;</td>
+                                </tr>
+                              </table>
+                              </form>
+                              </td>
+                              </tr>
+                            <tr>
+                              <td>Result Format</td>
+                            </tr>
+                            <tr>
+                              <td><table width="100%" border="1" cellspacing="1" cellpadding="1">
+                                <tr>
+                                  <td colspan="14" class="Result-Head-01"><strong>Exam : CBSE | Test : Flash Back Test 1 | Exam Date : 17/08/2008</strong></td>
+                                </tr>
+                                <tr>
+                                  <td><span class="row-01-td">Roll Number</span></td>
+                                  <td><span class="row-01-td">Rank</span></td>
+                                  <td><span class="row-01-td">Physics Rank</span></td>
+                                  <td><span class="row-01-td">Physics Marks</span></td>
+                                  <td><span class="row-01-td">Physics 
+                                    %</span></td>
+                                  <td><span class="row-01-td">Chem Rank</span></td>
+                                  <td><span class="row-01-td">Chem Marks</span></td>
+                                  <td><span class="row-01-td">Chem
+                                    %</span></td>
+                                  <td><span class="row-01-td">Bio Rank</span></td>
+                                  <td><span class="row-01-td">Bio Marks</span></td>
+                                  <td><span class="row-01-td">Bio
+                                    %</span></td>
+                                  <td><span class="row-01-td">Total Marks</span></td>
+                                  <td><span class="row-01-td">% of Marks</span></td>
+                                  <td><span class="row-01-td">% of %age</span></td>
+                                  </tr>
+                                  <tr>
+                                    <td><span class="row-01-td">9000117</span></td>
+                                    <td><span class="row-01-td">76</span></td>
+                                    <td><span class="row-01-td">33</span></td>
+                                    <td><span class="row-01-td">43</span></td>
+                                    <td><span class="row-01-td">8</span></td>
+                                    <td><span class="row-01-td">16</span></td>
+                                    <td><span class="row-01-td">12</span></td>
+                                    <td><span class="row-01-td">12</span></td>
+                                    <td><span class="row-01-td">10</span></td>
+                                    <td><span class="row-01-td">12</span></td>
+                                    <td><span class="row-01-td">56</span></td>
+                                    <td><span class="row-01-td">56</span></td>
+                                    <td><span class="row-01-td">17.78</span></td>
+                                    <td><span class="row-01-td">22.38</span></td>
+                                    </tr>
+                                  </table></td>
+                            </tr>
+                            </table></td>
+                        </tr>
+                        <tr>
+                          <td align="center"><img src="images/spacer.gif" width="600" height="2" alt="spacer" /></td>
+                        </tr>
+                        </table></td>
+                      <td valign="top">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td><script language="JavaScript1.2" src="assets/right-menu.js" type="text/javascript"></script>
+                              </td>
+                            </tr>
+                          </table>
+                        <img src="images/spacer.gif" width="150" height="2" alt="spacer" /></td>
+                    </tr>
+                    <tr class="footer">
+                      <td>&nbsp;</td>
+                      <td><script src="assets/footer-info-creator.js" type="text/javascript"></script></td>
+                      <td>&nbsp;</td>
+                    </tr>
+                    </table></td>
+                </tr>
+              </table></td>
+            </tr>
+          </table></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr align="center">
+        <td>&nbsp;</td>
+        <td><script src="assets/footer-info.js" type="text/javascript"></script></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><img src="images/spacer.gif" width="28" height="4" alt="spacer" /></td>
+        <td><img src="images/spacer.gif" width="912" height="2" alt="spacer" /></td>
+        <td><img src="images/spacer.gif" width="20" height="2" alt="spacer" /></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<script type="text/javascript">
+<!--
+swfobject.registerObject("FlashID");
+//-->
+</script>
+</body>
+</html>
