d12c4849d9ee79c98c5353d041e2d68cbcd38523
manaswin <member@manaswin.org>
2018-10-25 23:08:08 +0530

    Create header.php

---
+++ mw-admin/header.php
@@ -0,0 +1,20 @@
+<table width="780" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td align="left" valign="top" style="background-image:url(../mw-content/theme/default/image/header.gif); background-repeat:no-repeat;" height="100"><table width="780" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="210" height="100" align="left" valign="top"><table width="210" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td height="51">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="center" height="20" style="font-family:Tahoma, Geneva, sans-serif; font-size:11px; color:#000; font-weight:bold;">DASH BOARD</td>
+          </tr>
+          <tr>
+            <td align="center" valign="middle" style="font-family:Tahoma, Geneva, sans-serif; font-size:11px; color:#fff; font-weight:normal;" height="20">India's contribution to CMS community</td>
+          </tr>
+        </table></td>
+        <td width="570">&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
